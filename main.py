63e0c2f37fe44107ef6917c0f6b2229e4516883f
stefanrodrigues2 <stefanrodrigues.mec@gmail.com>
2018-03-28 21:16:17 +0530

    Program file add

---
+++ main.py
@@ -0,0 +1,14 @@
+largest, index = 0, -1
+for n in range(1,1000000):
+	count = 0
+	tmp = n
+	while (n>1):
+		if(n%2 == 0):
+			n = n/2
+		else:
+			n = 3*n + 1
+		count = count + 1
+	if(count>largest):
+		largest = count
+		index = tmp
+print("Largest count is %i at index %i"%(largest,index))
